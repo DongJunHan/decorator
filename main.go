e1673792d09d6a02f5c4f4e28b276a56a6cdfdb4
DongJunHan <gamedokdok@naver.com>
2021-03-07 18:31:24 +0900

    decorator pattern 복호화,압축푸는거 구현

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ type Component interface {
 }
 
 var sentData string
+var receiveData string
+
 type SendComponent struct {}
 
 func (self *SendComponent) Operator(data string){
@@ -46,13 +48,59 @@ func (self *EncryptComponent) Operator(data string){
 
 }
 
+type DecryptComponent struct{
+	key string
+	com Component
+}
+
+func (self *DecryptComponent) Operator(data string){
+	decryptData,err := cipher.Decrypt([]byte(data),self.key)
+	if err != nil {
+		panic(err)
+	}
+
+	self.com.Operator(string(decryptData))
+}
+
+type UnzipComponent struct{
+	com Component
+}
+
+func (self *UnzipComponent) Operator(data string){
+	unzipData,err := lzw.Read([]byte(data))
+	if err != nil{
+		panic(err)
+	}
+
+	self.com.Operator(string(unzipData))
+}
+
+type ReadComponent struct{}
+func (self *ReadComponent) Operator(data string){
+	receiveData = data
+}
+
 func main(){
+	//프로토콜 순서상 압축을 하고 암호화를 함.
+	//따라서 받는쪽에서는 복호화를 하고 압축을 풀어야함.
 	sender := &EncryptComponent{key : "abcde",
 		com : &ZipComponent{ 
-			com : &SendComponent{}}}
+			com : &SendComponent{},
+		},
+	}
 
 	sender.Operator("Hello world")
 	fmt.Println(sentData)
+
+	receiver := &UnzipComponent{
+		com : &DecryptComponent{
+			key : "abcde",
+			com : &ReadComponent{},
+		},
+	}
+
+	receiver.Operator(sentData)
+	fmt.Println(receiveData)
 }
 
 
